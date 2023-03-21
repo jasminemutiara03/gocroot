7e71de620514c94bd10d6b56643bed86192d075c
jasminemutiara03 <bjasminemutiara@gmail.com>
2023-03-21 14:15:24 +0700

    konekkan db

--- controller/controller.go
+++ controller/controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/aiteung/musik"
+	"github.com/aiteung/presensi"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 	"github.com/jasminemutiara03/gocroot/config"
@@ -26,6 +27,10 @@ func WsWhatsAuthQR(c *websocket.Conn) {
 func Homepage(c *fiber.Ctx) error {
 	ipaddr := musik.GetIPaddress()
 	return c.JSON(ipaddr)
+}
+func GetPresensiBulanIni(c *fiber.Ctx) error {
+	ps := presensi.GetPresensiCurrentMonth(config.Ulbimongoconn)
+	return c.JSON(ps)
 }
 func PostWhatsAuthRequest(c *fiber.Ctx) error {
 	if string(c.Request().Host()) == config.Internalhost {
--- url/url.go
+++ url/url.go
@@ -7,7 +7,8 @@ import (
 )
 
 func Web(page *fiber.App) {
-	page.Get("/", controller.Homepage)                                    //ujicoba panggil package musik
+	page.Get("/", controller.Homepage) //ujicoba panggil package musik
+	page.Get("/presensi", controller.GetPresensiBulanIni)
 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
 }
